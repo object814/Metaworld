cae6574a11417996cedc91605d26e2650b2afd4f
object814 <zhouhy0108@gmail.com>
2026-01-21 15:41:56 +0000

    Visualise using new wrappers and concatenated multiview cameras

--- scripts/visualise_tasks.py
+++ scripts/visualise_tasks.py
@@ -10,7 +10,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(BASE_DIR))
 import metaworld
-from metaworld.utils.obs_wrapper import ProprioImageObsWrapper, ProprioMultiImageObsWrapper
+from metaworld.wrappers import ProprioImageObsWrapper, ProprioMultiImageObsWrapper
 
 # choose task names
 PICK_ENV = "pick-place-v3"
@@ -43,7 +43,7 @@ def render_episode_multi_camera(env_name, out_path="out.gif", episode_length=500
     env = ProprioMultiImageObsWrapper(env,
                                       image_height=128,
                                       image_width=128,
-                                      camera_names=["topview", "corner", "front"])
+                                      camera_names=["topview", "front", "gripperPOV"])
     obs, info = env.reset()
     frames = []
     time_stamp = time.time()
@@ -54,9 +54,15 @@ def render_episode_multi_camera(env_name, out_path="out.gif", episode_length=500
             action = np.array([0.02, -0.02, 0.01, 0.1])
         obs, reward, terminated, truncated, info = env.step(action)
         print(f"EE Pos: {obs['proprio'][0]:.3f}, {obs['proprio'][1]:.3f}, {obs['proprio'][2]:.3f}, EE velocity: {obs['proprio'][3]:.3f}, {obs['proprio'][4]:.3f}, {obs['proprio'][5]:.3f}, Gripper Val: {obs['proprio'][6]:.3f}")
-        # frame = env.render()
-        # Combine multiple camera images side by side
-        combined_frame = np.concatenate(obs["images"], axis=1)
+        # image observations from multiple cameras are [H, W, 3*num_cameras]
+        img = obs["image"]
+        h, w, c = img.shape
+        num_cameras = c // 3
+        camera_frames = []
+        for i in range(num_cameras):
+            camera_frame = img[:,:, i*3:(i+1)*3]
+            camera_frames.append(camera_frame)
+        combined_frame = np.concatenate(camera_frames, axis=1)
         frames.append(combined_frame)
         if terminated or truncated:
             break
@@ -66,7 +72,7 @@ def render_episode_multi_camera(env_name, out_path="out.gif", episode_length=500
     print(f"Wrote {out_path} ({len(frames)} frames) in {time.time()-time_stamp:.2f} seconds.")
 
 if __name__ == "__main__":
-    render_episode(PICK_ENV, out_path="pick_place.gif", episode_length=100, random_actions=False)
-    render_episode(DRAWER_ENV, out_path="drawer_open.gif", episode_length=100, random_actions=False)
-    render_episode_multi_camera(PICK_ENV, out_path="pick_place_multi_cam.gif", episode_length=100, random_actions=False)
-    render_episode_multi_camera(DRAWER_ENV, out_path="drawer_open_multi_cam.gif", episode_length=100, random_actions=False)
+    render_episode(PICK_ENV, out_path="gifs/pick_place.gif", episode_length=100, random_actions=False)
+    render_episode(DRAWER_ENV, out_path="gifs/drawer_open.gif", episode_length=100, random_actions=False)
+    render_episode_multi_camera(PICK_ENV, out_path="gifs/pick_place_multi_cam.gif", episode_length=100, random_actions=False)
+    render_episode_multi_camera(DRAWER_ENV, out_path="gifs/drawer_open_multi_cam.gif", episode_length=100, random_actions=False)

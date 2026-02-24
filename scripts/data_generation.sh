4d2f8ae754c5b5f2428b6c13ae2bef004b5a97fa
Haoyu Zhou <zhouhy0108@gmail.com>
2026-02-24 15:09:46 +0000

    Add more data generation commands

--- scripts/data_generation.sh
+++ scripts/data_generation.sh
@@ -9,3 +9,27 @@ python generate_vjepa_dataset.py \
     --episode-length 100 \
     --output-dir /Metaworld/datasets/metaworld_draweropen \
     --env-name drawer-open-v3 \
+
+python generate_vjepa_dataset.py \
+    --num-episodes 500 \
+    --episode-length 300 \
+    --output-dir /Metaworld/datasets/metaworld_compo_draweropen_pickplace \
+    --env-name compo-draweropen-pickplace \
+
+python generate_vjepa_dataset.py \
+    --num-episodes 500 \
+    --episode-length 100 \
+    --output-dir /Metaworld/datasets/metaworld_door_open \
+    --env-name door-open-v3 \
+
+python generate_vjepa_dataset.py \
+    --num-episodes 500 \
+    --episode-length 100 \
+    --output-dir /Metaworld/datasets/metaworld_door_close \
+    --env-name door-close-v3 \
+
+python generate_vjepa_dataset.py \
+    --num-episodes 500 \
+    --episode-length 300 \
+    --output-dir /Metaworld/datasets/metaworld_compo-dooropen-doorclose \
+    --env-name compo-dooropen-doorclose \
